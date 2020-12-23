7d4bac5e13f2e1288ff19388628ca3dc0b2fea46
unknown <alvin.ictn@gmail.com>
2020-12-24 04:04:11 +0700

    add variables

---
+++ 002. Working with variables/Working with variables/Working with variables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Working_with_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intData; // use to declare integer number
+            double doubleData; // use to declare deciaml number
+            string stringData; // use to declare character or string
+
+        }
+    }
+}
